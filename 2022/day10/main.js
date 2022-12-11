03fa0814a114116a705872c73b7d3909436394fd
jeremie <jeremie.jade@pix.fr>
2022-12-11 01:17:41 +0100

    Day 10 etape 2

--- 2022/day10/main.js
+++ 2022/day10/main.js
@@ -3,7 +3,8 @@ const _ = require('lodash')
 const input = fs.readFileSync('./input', { encoding: 'utf8' });
 let cycle = 0;
 let x = 1;
-const xstate = []
+const print = []
+
 input.trim().split('\n')
   .map(l => {
     return [l.slice(0, 1), parseInt(l.slice(4, l.length))]
@@ -18,12 +19,24 @@ input.trim().split('\n')
   });
 
 function addCycle(cycle) {
-  cycle ++;
-  if((cycle - 20 )% 40 === 0) {
-    xstate.push(x * cycle)
+
+  const newCycle = cycle%40
+  if(newCycle >= x - 1 &&  newCycle <= x+ 1){
+    print.push('#')
+  }else {
+    print.push('.')
   }
+  cycle ++;
   return cycle;
 }
 
+const screen = print.reduce((acc, p, i)=>{
+  if(i%40 === 0){
+    acc+='\n'
+  }
+  acc+=p
+  return acc
+}, '')
 
-console.log(_.sum(xstate))
+
+console.log(screen)
