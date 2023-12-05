a979f39a2576e92224955182cb06a6f54c2e42f0
jeremie <jeremie.jade@pix.fr>
2023-12-05 21:39:29 +0100

    Day5-2

--- 2023/day5/main.js
+++ 2023/day5/main.js
@@ -7,13 +7,47 @@ fs.readFile('./input', 'utf8',(err, data) => {
   almanacCategories = almanacCategories.map(almanacCat => {
     return {
       cat: almanacCat,
-      content: buildCorrespondence(almanacCat, input).map(c => c.split(' ').map(n => parseInt(n)))
+      content: buildCorrespondence(almanacCat, input)
+        .map(c => c
+          .split(' ')
+          .map(n => parseInt(n)))
+        .sort(([,startA],[,startB]) => startA - startB)
     }
   });
-  let seed = almanacCategories.shift();
-  const result = seed.content[0].map(s => findLocationFromSeed(s, almanacCategories))
+  let seeds = almanacCategories.shift();
 
+  seeds = seeds.content[0].reduce((acc, _, i) => {
+    if(i%2 !== 0) {
+      acc.push({
+        start: seeds.content[0][i - 1],
+        length: seeds.content[0][i]
+      });
+    }
+    return acc
+  },[])
+    .sort(sortSeed)
+  console.log('seeds', seeds)
+
+  const result = almanacCategories.reduce((acc, { content }, index) => {
+    console.log(`
+==============   ${index}~~~~${almanacCategories[index].cat}~~~~${index}    ===================
+    `)
+    acc = getNextCatRange(
+      {
+        seedStart: acc[0].start,
+        seedLength: acc[0].length,
+        seeds: acc,
+        contentContext: content[0],
+        content,
+      })
+      .sort(sortSeed);
+    console.log('reuslt =====>', acc)
+    return acc
+  },[...seeds])
+  // console.log(result)
   console.log('result', result)
+
+  // console.log('almanacCategories', almanacCategories)
 
 });
 
@@ -24,7 +58,271 @@ function buildCorrespondence(cat, input) {
   return content.splice(start + 1, end-start - 1);
 }
 
-function findLocationFromSeed(seed, almanacCategories){
+function getNextCatRange(
+  {
+   seedStart,
+   seedLength,
+   seedIndex = 0,
+   seeds,
+   contentContext: [next, catStart, catLength],
+   content,
+   index = 0,
+   nextCatRanges = []
+}
+) {
+ /* console.log(`
+-------------> ${seedIndex}-${index}
+  `)
+  console.log({next, catStart, catLength})
+  console.log( {seedStart, seedLength})*/
+  if(seedStart === 74) {
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  }
+  console.log('--------------********-------------')
+  console.log('--------------========-------------')
+
+  const { action, nextCatRange } = computeNextCatRange(seedStart, seedLength ,[next, catStart, catLength]);
+
+
+  if(nextCatRange) nextCatRanges.push(nextCatRange)
+
+  console.log('content',content)
+  console.log(action.name)
+  console.log({next, catStart, catLength})
+  console.log( {seedStart, seedLength})
+
+  console.log('--------------========-------------')
+  console.log('--------------********-------------')
+
+
+  if(action.name === 'end') {
+    if(seeds.length > seedIndex + 1) {
+
+      nextCatRanges.push(...seeds.splice(seedIndex + 1, seeds.length))
+    }
+    return nextCatRanges;
+  }
+  if(action.name === 'continue') {
+    return getNextCatRange(
+      {
+        seedStart: catStart,
+        seedLength: seedLength - action.length,
+        seedIndex,
+        seeds,
+        contentContext: content[index],
+        content,
+        index,
+        nextCatRanges
+      });
+  }
+  if(action.name === 'cat') {
+    index++;
+
+    if(!content[index]) content[index]= new Array(3)
+    return getNextCatRange(
+      {
+        seedStart: seedStart + catLength,
+        seedLength: action.length,
+        seedIndex,
+        seeds,
+        contentContext: content[index],
+        content,
+        index,
+        nextCatRanges
+      });
+  }
+  if(action.name === 'cat-sup') {
+    index++;
+
+    if(!content[index]) content[index]= new Array(3)
+    return getNextCatRange(
+      {
+        seedStart:  catStart + catLength,
+        seedLength: action.length,
+        seedIndex,
+        seeds,
+        contentContext: content[index],
+        content,
+        index,
+        nextCatRanges
+      });
+  }
+  if(action.name === 'empty-cat') {
+    index++;
+    if(!content[index]) content[index]= new Array(3)
+    return getNextCatRange(
+      {
+        seedStart: seedStart,
+        seedLength: action.length,
+        seedIndex,
+        seeds,
+        contentContext: content[index],
+        content,
+        index,
+        nextCatRanges
+      });
+  }
+  if(action.name === 'seed') {
+    seedIndex++;
+    if(!seeds[seedIndex]) return nextCatRanges;
+    return getNextCatRange(
+      {
+        seedStart: seeds[seedIndex].start,
+        seedLength: seeds[seedIndex].length,
+        seedIndex,
+        seeds,
+        contentContext: [next + seedLength, catStart + seedLength, action.length],
+        content,
+        index,
+        nextCatRanges
+      });
+  }
+  if(action.name === 'seed-sup') {
+    seedIndex++;
+    if(!seeds[seedIndex]) return nextCatRanges;
+    return getNextCatRange(
+      {
+        seedStart: seeds[seedIndex].start,
+        seedLength: seeds[seedIndex].length,
+        seedIndex,
+        seeds,
+        contentContext: [next + (seedStart-catStart) + seedLength, catStart + (seedStart-catStart)  + seedLength, action.length],
+        content,
+        index,
+        nextCatRanges
+      });
+  }
+  if(action.name === 'cat-seed' || action.name === 'cat-seed-sup') {
+    seedIndex++;
+    if(!seeds[seedIndex]) return nextCatRanges;
+    index++;
+    if(!content[index]) content[index]= new Array(3)
+
+    return getNextCatRange(
+      {
+        seedStart: seeds[seedIndex].start,
+        seedLength: seeds[seedIndex].length,
+        seedIndex,
+        seeds,
+        contentContext: content[index],
+        content,
+        index,
+        nextCatRanges
+      });
+  }
+}
+
+function computeNextCatRange(seedStart, seedLength, [next, catStart, catLength]) {
+
+  console.log({
+    seedLength,
+    seedStart,
+    catStart,
+    catLength
+  })
+  if(seedStart + seedLength < catStart || next === undefined) {
+    return {
+      action: {
+        name: 'end'
+      },
+      nextCatRange:{
+        start: seedStart,
+        length: seedLength
+      }
+    }
+  }
+  if(seedStart > catStart + catLength) {
+    return {
+      action: {
+        name: 'empty-cat',
+        length: seedLength
+      },
+      nextCatRange: null
+    }
+  }
+  if(seedStart < catStart) {
+    console.log('<')
+
+    return {
+      action: {
+        name: 'continue',
+        length: catStart - seedStart
+      },
+      nextCatRange:{
+        start: seedStart,
+        length: catStart - seedStart
+    }
+    };
+  }
+  if(seedStart === catStart) {
+    console.log('=')
+
+    if(seedLength > catLength) {
+      return {
+        action: {
+          name: 'cat',
+          length: seedLength - catLength
+        },
+        nextCatRange: {
+          start: next,
+          length: catLength
+        }
+      }
+    }
+    return {
+      action: {
+        name: seedLength === catLength ? 'cat-seed' : 'seed',
+        length: catLength - seedLength
+      },
+      nextCatRange: {
+        start: next,
+        length: seedLength
+      }
+    }
+  }
+
+  if(seedStart > catStart) {
+    console.log('>')
+
+    if(seedStart + seedLength > catStart + catLength) {
+      console.log('actoin cat')
+
+      return {
+        action: {
+          name: 'cat-sup',
+          length: seedLength - ((catStart + catLength) - seedStart)
+        },
+        nextCatRange: {
+          start: next + (seedStart-catStart),
+          length: catLength
+        }
+      }
+    }
+    console.log('actoin seed-sup', seedStart + seedLength === catStart + catLength ? 'cat-seed-sup' : 'seed-sup')
+    console.log({
+      start: next + (seedStart-catStart),
+      length: catLength,
+      seedLength
+    })
+    return {
+      action: {
+        name:  seedStart + seedLength === catStart + catLength ? 'cat-seed-sup' : 'seed-sup',
+        length: catLength - seedLength - (seedStart-catStart)
+      },
+      nextCatRange: {
+        start: next + (seedStart-catStart),
+        length: seedLength
+      }
+    }
+  }
+
+}
+
+function sortSeed ({start: startA}, {start:startB}) {
+ return startA - startB
+}
+
+/*function findLocationFromSeed(seed, almanacCategories){
   let location = seed;
   almanacCategories.forEach(({content}) => {
     location = findNextCat(location, content);
@@ -41,4 +339,4 @@ function findNextCat(cat, content) {
     }
   });
   return nextCat
-}
+}*/
