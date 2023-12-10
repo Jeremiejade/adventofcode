4cae49c97ae317d745d73710546e580063d23b19
jeremie <jeremie.jade@pix.fr>
2023-12-10 22:20:56 +0100

    day10-2

--- 2023/day10/main.js
+++ 2023/day10/main.js
@@ -12,14 +12,14 @@ const PIPES = {
 
 const COORDINATE_BY_PIPE = {}
 
-
 fs.readFile('./input', 'utf8',(err, data) => {
  const fData =  data.toString().trim().split('\n')
    .map(line => line.split(''));
   const start = buildData(fData);
   const direction =  findStartPipe(start, fData);
-  const result = findLoopLength(start.y, start.x, direction, fData, start);
-  console.log(result/2)
+  const loop = BuildLoop(start.y, start.x, direction, fData, start);
+  const result = computeTilesNUmber(loop);
+  console.log(result)
 });
 
 function buildData(field) {
@@ -56,22 +56,32 @@ function findStartPipe(start, fData) {
   return  top ? 0 : right ? 1 : 2
 }
 
-function findLoopLength(y, x, direction, fData, start) {
+function BuildLoop(y, x, direction, fData, start) {
   let round = 0
+  const loop = Array(fData.length)
   while (true) {
     if(start.y === y  && start.x === x && round !== 0) {
     break
     }
+    if(!loop[y]) loop[y] = []
     const pipe = fData[y][x];
     const next = PIPES[pipe][direction]
+
     const {nY, nX, nDirection} = nextPipe(y, x, next);
+    loop[y].push({x, direction: direction === 0 || direction === 2 ? direction : nDirection})
+
     y = nY;
     x = nX;
     direction = nDirection;
     round++
 
   }
-  return round
+  loop.forEach((line) => {
+    line.sort((a, b) => a.x - b.x)
+  })
+  return loop
+    .filter(line => !!line)
+
 }
 
 function nextPipe(y,x,next) {
@@ -104,3 +114,72 @@ function nextPipe(y,x,next) {
     }
   }
 }
+
+function computeTilesNUmber(loop) {
+  loop.shift();
+  return loop.reduce((acc, line, y) => {
+    return acc + line.reduce((acc, {x, direction}, index) => {
+      const nextTile = line[index + 1];
+
+      if(!nextTile || nextTile.x === x + 1 || direction !== 0) {
+        return acc
+      }
+      return acc + (nextTile.x - x -1);
+    }, 0);
+  }, 0)
+}
+/*
+function findEnclosedTiles(loop) {
+  const visited = {};
+  const top = loop.findIndex(l=> !!l)
+  const bottom = loop.reverse().findIndex(l=> !!l)
+  for (let i = top + 1; i < bottom; i++) {
+    const lines = loop[i]
+    lines.forEach(({x, pipe}, j) => {
+      if(j === lines.length) {
+        return
+      }
+      if(lines[j+1] !== x+1){
+
+      }
+
+    })
+  }
+}
+
+function findTiles(y, x, top, bottom, loop) {
+  const tiles = {};
+  const pathBetweenPipe = {}
+
+  let isEnclosed = false
+  while (true) {
+
+    if(tiles[`${y}-${x}`]){
+      isEnclosed = true
+      break
+    }
+    tiles[`${y}-${x}`] = s
+    // canU up
+
+    if (y === top) {
+      break
+    }
+    const nextUp = loop[y -1].find(n => n.x === x)
+    if(!!nextUp){
+      y = y - 1
+      continue
+    }
+    if(nextUp.pipe === 'J') {
+      findPathBetweenPipe()
+    }
+
+
+    // can u right
+
+    // can u down
+
+    // can u left
+  }
+
+
+}*/
