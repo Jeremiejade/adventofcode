1eea21762a62cb2eaa432cc83b1bca040b63a4eb
jeremie <jeremie.jade@pix.fr>
2023-12-11 19:13:26 +0100

    day-2

--- 2023/day11/main.js
+++ 2023/day11/main.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 fs.readFile('./input', 'utf8',(err, data) => {
  const fData =  data.toString().trim().split('\n')
    .map(line => line.split(''));
- const skyMap = doubleEmptyLineAndEmptyColumn(fData)
-  const galaxies = findGalaxies(skyMap)
-
+  const galaxies = findGalaxies(fData);
+  const emptyLineIndex = findEmptyLineIndex(fData);
+  const emptyColumnIndex = findEmptyColumnIndex(fData)
+  const spaceExpand = 1000000;
   const result = galaxies.reduce((acc, {x, y}, i)=> {
     if( i+1 ===  galaxies.length) return acc
     for (let j = i+ 1; j < galaxies.length; j++) {
-      const otherGalaxy = galaxies[j];
-      acc += difference(otherGalaxy.y, y) + difference(otherGalaxy.x, x)
+      const g = getCoordinatesBySpaceExpand({x, y, spaceExpand, emptyLineIndex, emptyColumnIndex})
+      const otherGalaxy = getCoordinatesBySpaceExpand({...galaxies[j], spaceExpand, emptyLineIndex, emptyColumnIndex});
+      acc += difference(otherGalaxy.y, g.y) + difference(otherGalaxy.x, g.x)
     }
     return acc
   }, 0)
@@ -21,31 +23,41 @@ function difference(a, b) {
   return Math.abs(a - b);
 }
 
-function doubleEmptyLineAndEmptyColumn(fData) {
-  const skyMap = fData.reduce((acc, _, i) => {
-    const index = fData.length - 1 - i
-    const line = fData[index]
-    if(line.indexOf('#') === -1){
-      acc.push(line);
-    }
-    acc.push(line);
-    return acc;
-  },[]);
-  return  doubleEmptyColumn(skyMap.reverse())
+function getCoordinatesBySpaceExpand({x, y, spaceExpand, emptyLineIndex, emptyColumnIndex}) {
+  return {
+    x: getIndexBySpaceExpand(x, emptyColumnIndex, spaceExpand),
+    y: getIndexBySpaceExpand(y, emptyLineIndex, spaceExpand)
+  }
 }
 
-function doubleEmptyColumn(fData) {
-  const line = [...fData[0]];
+function getIndexBySpaceExpand(index, emptySpaceIndex, spaceExpand) {
+  return emptySpaceIndex.reduce((acc, es) => {
+    if(es < index) {
+      return acc + spaceExpand - 1
+    }
+    return acc;
+  }, index)
+}
+
+function findEmptyLineIndex(fData) {
+  return fData.reduce((acc, line, i) => {
+    if(line.indexOf('#') === -1){
+      acc.push(i);
+    }
+    return acc;
+  },[]);
+}
+
+function findEmptyColumnIndex(fData) {
+  const line = fData[0];
   return line.reduce((acc, _, i) => {
-    const index = line.length - 1 - i
-    if(fData.every(line => line[index] === '.')){
-      acc = acc.map((line) => {
-        line.splice(index, 0, '.');
-        return line
-      });
+    if(fData.every(line => {
+      return line[i] === '.'
+    })){
+      acc.push(i);
     }
     return acc
-  },[...fData])
+  },[]);
 }
 
 function findGalaxies(skyMap) {
