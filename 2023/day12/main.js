06dffbb6749bf6c7402a5b8d666d9aedfe2e1977
jeremie <jeremie.jade@pix.fr>
2023-12-13 02:07:31 +0100

    day12-2 try

--- 2023/day12/main.js
+++ 2023/day12/main.js
@@ -1,58 +1,143 @@
 const fs = require('fs');
 
-let allPossibilities = []
+let allPossibilities = 0
+let allPossibilityMemory = []
 
 fs.readFile('./input', 'utf8',(err, data) => {
- const fData =  data.toString().trim().split('\n')
+  data.toString().trim().split('\n')
    .map(line => line.split(' '))
    .map(([row, damagedGroup])=>{
      return [row, damagedGroup.split(',').map(n => parseInt(n))]
-   });
-  const r = fData.reduce((acc, [row, damagedGroup]) => {
-      return acc + findArrangements(row, damagedGroup)
-  }, 0)
-  console.log(r)
+   })
+   .forEach(([row, damagedGroup])=> {
+     const newRow = getNewRow(row)
+     const newDamagedGroup = getNewDamagedGroup(damagedGroup)
+     const firstDamagedGroup = newDamagedGroup[0];
+     const regex = buildRegex(firstDamagedGroup, 0)
+     console.log(newDamagedGroup, newRow)
+     findAllPossibilities(newRow, newDamagedGroup, firstDamagedGroup, regex)
+   })
+
+
+  console.log(allPossibilities)
 });
 
+/*
 function findArrangements(row, damagedGroup) {
-  allPossibilities = []
-  findAllPossibilities(row.split(''))
-  let regex = damagedGroup.reduce((acc, d, i)=> {
-    if(i === damagedGroup.length -1){
-      return acc + `\\#{${d}}`
-    }
-    return acc + `\\#{${d}}\\.+`
-  }, '');
-  regex = new RegExp(`(^\\.+${regex}\\.+$)|(^\\.+${regex}$)|(^${regex}\\.+$)|(^${regex}$)`, 'g')
-  const result =  allPossibilities.reduce((acc, possibility)=> {
-    const r = findArrangementsByType(possibility, regex)
-    if(r) acc.push(r)
-    return acc
-  }, [])
-  return result.length
-}
-
-function findArrangementsByType(row, regex) {
-  const r = [...row.matchAll(regex)]
+  const broken = [...row.matchAll(/(#*)/g)]
     .filter(([r])=> !!r)
-  if(r.length) {
-    return {r: r[0][0], i:r[0].index}
+    .map((r)=> {
+      return {
+        group: r[0],
+        index: r.index
+      }
+    });
+/!*  const minLengthMatch = damagedGroup.reduce((acc, g) => {
+    return acc + g;
+  },0) + broken.length - 1;*!/
+const firstDamagedGroup = damagedGroup[0];
+  const regex = buildRegex(firstDamagedGroup)
+  console.log('reg', regex)
+  findAllPossibilities(row, damagedGroup, firstDamagedGroup, regex)
+  console.log(allPossibilities)
+
+
+}
+*/
+
+function getNewRow(row) {
+  const newRow = []
+  for (let i = 0; i < 5; i++) {
+    newRow.push(row)
   }
-  return null
+  return newRow.join('?')
 }
 
-function findAllPossibilities(row, index = 0, possibility = []) {
-  if(index > row.length - 1) {
-    allPossibilities.push(possibility.join(''))
-    return;
+function getNewDamagedGroup(damagedGroup) {
+  const newDamagedGroup = []
+  for (let i = 0; i < 5; i++) {
+    newDamagedGroup.push(...damagedGroup)
   }
+  return newDamagedGroup
+}
+
+
+
+function findAllPossibilities(row, damagedGroup, d, {regex, regexpRealMatch}, damagedGroupIndex = 0, index = 0, possibility = '') {
   const piece = row[index];
   index++
-  if(piece === '.' || piece === '#') {
-    possibility.push(piece)
-    findAllPossibilities(row, index, possibility)
-  }else {
-    findAllPossibilities(row, index, [...possibility, '.'])
-    findAllPossibilities(row, index, [...possibility, '#'])
+
+  const partialMatch = possibility.match(regex) !== null;
+  const totalMatch = possibility.match(regexpRealMatch) !== null
+  if((partialMatch && !totalMatch) || possibility === '' ) {
+    if (!piece) return
+
+    if(piece === '.' || piece === '#') {
+      possibility+= piece;
+      findAllPossibilities(row, damagedGroup, d, {regex, regexpRealMatch}, damagedGroupIndex, index, possibility)
+    }else {
+      findAllPossibilities(row, damagedGroup, d, {regex, regexpRealMatch}, damagedGroupIndex, index, possibility + '.');
+      findAllPossibilities(row, damagedGroup, d, {regex, regexpRealMatch}, damagedGroupIndex, index, possibility + '#');
+    }
+    return
   }
+  if(!totalMatch) return
+
+
+  if(damagedGroupIndex === damagedGroup.length - 1 ) {
+    const rest = row.substring(index-1)
+    if(rest.indexOf('#') === -1) {
+      allPossibilities++
+    }
+    return
+  }
+  damagedGroupIndex++;
+  const newD = damagedGroup[damagedGroupIndex]
+  const newRegex = buildRegex(newD, damagedGroupIndex)
+
+  if (!piece) return
+  if(piece === '.' || piece === '#') {
+    findAllPossibilities(row, damagedGroup, newD, newRegex, damagedGroupIndex, index, piece)
+  }else {
+    findAllPossibilities(row, damagedGroup, newD, newRegex, damagedGroupIndex, index, '.');
+    findAllPossibilities(row, damagedGroup, newD, newRegex, damagedGroupIndex, index, '#');
+  }
+
+}
+
+function buildRegex(d, index) {
+  let regex = '(^\\.+)';
+  let regexpRealMatch;
+  if(d === 1) {
+    if(index !== 0){
+      return  {
+        regex: new RegExp('(^\\.+$)', 'g'),
+        regexpRealMatch: new RegExp(`(^\\.+#$)`, 'g'),
+      }
+    }
+   return  {
+     regex: new RegExp('(^\\.+$)', 'g'),
+     regexpRealMatch: new RegExp(`(^#$)|(^\\.+#$)`, 'g'),
+    }
+
+  }
+  if(index !== 0) {
+    for (let i = 1; i <= d; i++) {
+      if(d === i) {
+        regexpRealMatch =  new RegExp(`(^\\.+#{${i}}$)`, 'g')
+      }else {
+        regex = `(^\\.+#{${i}}$)|` + regex
+      }
+    }
+    return { regex: new RegExp(regex, 'g'), regexpRealMatch}
+  }
+
+  for (let i = 1; i <= d; i++) {
+    if(d === i) {
+      regexpRealMatch =  new RegExp(`(^#{${i}}$)|(^\\.+#{${i}}$)`, 'g')
+    }else {
+      regex = `(^#{${i}}$)|(^\\.+#{${i}}$)|` + regex
+    }
+  }
+  return { regex: new RegExp(regex, 'g'), regexpRealMatch}
 }
