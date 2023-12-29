f33e25309497d3dee2d1194758c90f922bb0046f
jeremie <jeremie.jade@pix.fr>
2023-12-29 14:25:55 +0100

    day15-2

--- 2023/day15/main.js
+++ 2023/day15/main.js
@@ -2,9 +2,20 @@ const fs = require('fs');
 const _ = require("lodash");
 
 fs.readFile('./input', 'utf8',(err, data) => {
-  const fData = data.toString().trim().split(',')
-  const result = fData.reduce((acc, code) => {
-    return acc + computeCode(code)
+  const fData = data.toString().trim().split(',');
+  const boxes = fData.reduce((boxes, code) => {
+    const label = code.match(/\w+/)[0]
+    const boxNumber = computeCode(label)
+    if (code.indexOf('=') !== -1){
+      const lens = parseInt(code.match(/\d+/)[0])
+      return addLensInBox(boxes, boxNumber, label, lens)
+    }
+    return removeLensInBox(boxes, boxNumber, label)
+  },new Array(256));
+  const result = boxes.reduce((acc, box, index)=> {
+    return acc + box.reduce((acc, [,lens], j) => {
+      return acc + ((index + 1) * (j + 1) * lens)
+    },0)
   },0)
 
   console.log(result)
@@ -15,6 +26,39 @@ function computeCode(code) {
   for (let i = 0; i < code.length; i++) {
     result = (result + code.charCodeAt(i)) * 17 % 256
   }
-  console.log({code, result})
   return result
+}
+
+function removeLensInBox(boxes, boxNumber, label) {
+  if(boxes[boxNumber]) {
+    boxes[boxNumber] = boxes[boxNumber].filter(([l]) => l !== label)
+  }
+  return boxes
+}
+
+function addLensInBox(boxes, boxNumber, label, lens) {
+  if(!boxes[boxNumber]){
+    boxes[boxNumber] = [[label, lens]]
+    return boxes
+  }
+  const sameLabelBoxIndex = findLabelBox(label, boxes[boxNumber]);
+  if (sameLabelBoxIndex !== null) {
+    boxes[boxNumber][sameLabelBoxIndex][1] = lens;
+    return boxes
+  }
+  boxes[boxNumber].push([label, lens])
+  return boxes
+}
+
+function findLabelBox(label, labels) {
+  let index = null;
+
+  labels.find((l, i) => {
+    if (l[0] === label) {
+      index = i
+      return true;
+    }
+    return false;
+  });
+  return index;
 }
